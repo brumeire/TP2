0db912f4f5dd97f6fdc3218fd82b17a68c84bd71
JARTRON\Skaje <Skaje@hotmail.fr>
2017-02-13 14:37:46 +0100

    Statut base

---
+++ TP2/Assets/Scripts/Statut.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statut {
+
+	public string name;
+	public int age;
+	public enum sexe {male, female, castrated_male, castrated_female};
+	public int ID;
+	public enum ask {vaccin, castration, identify};
+
+	public void OnScreenStatut (){
+
+
+	}
+
+}
