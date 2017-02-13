93f198a5866dee7b2fb24055506602c1da2aa86b
lmoge <ludovicmoge.ens@gmail.com>
2017-02-13 14:49:18 +0100

    KitKat

---
+++ TP2/Assets/Scripts/Cat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat
+{
+    protected const float maxAnxiety = 100.0f;
+    protected float actualAnxiety = 0.0f;
+    protected float anxietySpeed = 0.1f;
+
+    public bool anxious = false;
+    public bool catched = false;
+    public float moveSpeed = 5.0f;
+    
+
+    public Cat()
+    {
+
+    }
+
+    /// <summary>
+    /// Execute this the normal update of a monobehaviour class.
+    /// </summary>
+    public virtual void UpdateCat()
+    {
+        if (!anxious)
+        {
+            actualAnxiety += anxietySpeed * Time.deltaTime;
+            if (actualAnxiety >= maxAnxiety)
+            {
+                anxious = true;
+            }
+        }
+        else if (catched)
+        {
+
+        }
+
+    }
+
+    public virtual void Catch()
+    {
+        catched = !catched;
+    }
+}
